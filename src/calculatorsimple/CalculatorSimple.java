cda3a1f6057c448a445d7aedbd846f8e87d14e2f
AshiqueAliMahar <40056794+AshiqueAliMahar@users.noreply.github.com>
2019-10-22 13:20:39 +0500

    simple with five basic operations

---
+++ src/calculatorsimple/CalculatorSimple.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorsimple;
+
+/**
+ *
+ * @author Ashiq
+ */
+public class CalculatorSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CalculatorFrame ob=new CalculatorFrame();
+        ob.setVisible(true);
+        
+        
+    }
+    
+}
